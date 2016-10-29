fb06393cf46988b02ef131a540427dd1324b548f
esotericabsurdist <esotericabsurdist@gmail.com>
2016-10-29 13:28:55 -0700

    Added Tony's homepage

--- public/javascripts/costumes.js
+++ public/javascripts/costumes.js
@@ -1,114 +1,30 @@
 /*
  *
- *    costume.js
+ *    costumes.js
  *
  */
 //==============================================================================
 $(document).ready(function() // wait for DOM to be ready
 {
 
+    //============================================================================
+    $.get('/retreivecostume', function(costumeData){
+      // dummy data
+      var costumePath = costumeData.costumePath;
+      var costumeTitle = costumeData.costumeTitle;
+      var costumeDescription = costumeData.costumeDescription;
 
-  /* TODO */
+      document.getElementById('costumeImage').src = costumePath;
+      document.getElementById('costumeDescription').innerHTML = costumeDescription;
+      document.getElementById('costumeTitle').innerHTML = costumeTitle;
 
-  $.get('/retreivecostume', function(costumeData){
-      // set costume data into html elements.
-
-      
-
-  });
-
-
-  /* TODO */
-  /*
-     implement logic to handle comments and rating,
-  */
+      // loop through comment data from costumeData and set in view
+      // document.getElementById('costumeComments')
+    });
+  //============================================================================
+    // Submit comment:
+  var submitCommmentButton = document.getElementById('submitCommmentButton')
+    submitCommmentButton.addEventListener('click', function() { /*TODO*/}, false);
+  //============================================================================
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /* This is old test code. Do not delete.
-  //============================================================================
-  // GET JSON type image metadata from api to reference and load a costume image on page load.
-
-  $.get( "/images", function( data ) {
-
-    // get a random index.
-    var randomIndex = Math.floor(Math.random() * (data.imagesJSON.length));
-
-    // get the path.
-    var imgPath = data.imagesJSON[randomIndex].imgPath;
-
-    // get the file name.
-    var imgName = data.imagesJSON[randomIndex].imgName;
-
-    // Set image src using file name.
-    document.getElementById('costume_image').src = imgPath;
-    //document.getElementById('costume_description').innerHTML = imgName;
-  });
-  //============================================================================
-
-
-  //============================================================================
-  // Define like button behavior
-  var likeButton = document.getElementById('like_button')
-  likeButton.addEventListener('click', function() {
-
-    // Get another random image for now.
-    $.get( "/images", function( data ) {
-
-      // get a random index.
-      var randomIndex = Math.floor(Math.random() * (data.imagesJSON.length));
-
-      // get the path.
-      var imgPath = data.imagesJSON[randomIndex].imgPath;
-
-      // get the file name.
-      var imgName = data.imagesJSON[randomIndex].imgName;
-
-      // Set image src using file name.
-      document.getElementById('costume_image').src = imgPath;
-      //document.getElementById('costume_description').innerHTML = imgName;
-    });
-
-
-  }, false);
-  //============================================================================
-
-
-
-  // Define dislikeButton behavior
-  var dislikeButton = document.getElementById('dislike_button')
-  dislikeButton.addEventListener('click', function() {
-
-
-    // Get another random image for now.
-    $.get( "/images", function( data ) {
-
-      // get a random index.
-      var randomIndex = Math.floor(Math.random() * (data.imagesJSON.length));
-
-      // get the path.
-      var imgPath = data.imagesJSON[randomIndex].imgPath;
-
-      // get the file name.
-      var imgName = data.imagesJSON[randomIndex].imgName;
-
-      // Set image src using file name.
-      document.getElementById('costume_image').src = imgPath;
-      //document.getElementById('costume_description').innerHTML = imgName;
-    });
-  }, false);
-  //============================================================================
-  */
