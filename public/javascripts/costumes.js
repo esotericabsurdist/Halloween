27752ba857674ef098380869faacf09d1106db5b
esotericabsurdist <esotericabsurdist@gmail.com>
2016-10-31 12:03:32 -0700

    added Tony's star rating to costume.js and added jquery-bar-rating folder to public

--- public/javascripts/costumes.js
+++ public/javascripts/costumes.js
@@ -10,63 +10,128 @@ $(document).ready(function() // wait for DOM to be ready
     // GET a costme for the page:
     /* TODO Change to $.ajax... */
     $.get('/retreivecostume', function(costumeData){
-      // dummy data
+      // get costume data
       var costumePath = costumeData.imgPath;
       var costumeTitle = costumeData.title;
       var costumeDescription = costumeData.description;
+      var costumeTotalRating = costumeData.totalRating;
+      var costumeNumberRating = costumeData.numberRatings;
+      var costumeComments = new Array();
+      costumeComments = costumeData.comments;
 
+      // set image, description and title.
       document.getElementById('costumeImage').src = costumePath;
       document.getElementById('costumeDescription').innerHTML = costumeDescription;
       document.getElementById('costumeTitle').innerHTML = costumeTitle;
 
-      // loop through comment data from costumeData and set in view
-      // document.getElementById('costumeComments')
+
+
+      // loop through comment data from costumeData and set in view:
+      var commentSection = document.getElementById('costumeComments');
+      for(var i = 0; i < costumeComments.length; i++){
+      commentSection.innerHTML = commentSection.innerHTML +
+      "<div class=\"comment\">"+
+          "<div class=\"content\">"+
+              "<a class=\"author\">"+costumeComments[i].user+"</a>"+
+              "<div class=\"text\">"+
+                  "<p>"+costumeComments[i].message+"</p>"+
+              "</div>"+
+          "</div>"+
+      "</div>";
+      }
+
+
+      // Set rating into view.
+      var average = costumeTotalRating/costumeNumberRating;
+      // TODO set average as rating.
     });
 
-
-
-
-
-
   //============================================================================
-    // Submit comment and update comment section.
+  // Submit comment and update comment section.
   var submitCommmentButton = document.getElementById('submitCommmentButton')
     submitCommmentButton.addEventListener('click', function() {
 
-    var title = document.getElementById('costumeTitle').innerHTML;
-    var comment = $('#commentText').val();
+    var costumeTitle = document.getElementById('costumeTitle').innerHTML;
+    var costumeComment = $('#commentText').val();
 
     var postData = {
       rating: null,
-      message: comment,
-      costumeTitle: title
-    }
-    /*var costumeSchema = new Schema({
-      imgPath: String,
-      title:  String,
-      description: String,
-      userName: String,
-      totalRating: Number,
-      numberRatings: Number,
-      comments: [{ user:{type: String, default: 'anonymous'}, message: String}]
-    });*/
+      message: costumeComment,
+      title: costumeTitle
+      }
+
+
     $.ajax({
       url: '/updatecostume',
       type: 'POST',
       dataType: 'json',
-      data: JSON.stringify(postData);
-      success: function(err,costumeData) {
+      data: postData, // data: JSON.stringify(postData), <-- this addes an extra set of braces. doesn't work.
+      success: function(costumeData) {
         // Now that we have returned, add the comment to the view.
-        if(err){
-          console.log(err);
-        }
-        console.log("here is the response data: "+costumeData.title);
-        //TODO read the array of comments and rewrite all of them as newest to oldest.
+        var costumeComments = new Array();
+        costumeComments = costumeData.comments;
 
-        // remove current comments
-        var comments = document.getElementById('comments');
-        comments.empty();
+        // loop through comment data from costumeData.comments and set in view
+        var commentSection = document.getElementById('costumeComments').innerHTML = " ";
+        var commentSection = document.getElementById('costumeComments');
+        for(var i = 0; i < costumeComments.length; i++){
+          commentSection.innerHTML = commentSection.innerHTML +
+          "<div class=\"comment\">"+
+              "<div class=\"content\">"+
+                  "<a class=\"author\">"+costumeComments[i].user+"</a>"+
+                  "<div class=\"text\">"+
+                      "<p>"+costumeComments[i].message+"</p>"+
+                  "</div>"+
+              "</div>"+
+          "</div>";
+        }
       }}, false);
-  //============================================================================
   });
-});
+  //============================================================================
+  // update stars on star rating made by user? Is barrating an event listener that takes a call back function?
+  $('#rating').barrating({
+          theme: 'fontawesome-stars-o',
+          showSelectedRating: false,
+          initialRating: average,
+          onSelect: function(value, text) {
+              //destory and set the new average rating
+              $('#rating').barrating('destroy');
+
+              var postData = {
+                rating: value,
+                message: null,
+                title: costumeTitle
+              }
+
+              // send new rating value to server.
+              $.ajax({
+                url: '/updatecostume',
+                type: 'POST',
+                dataType: 'json',
+                data: postData,
+                success: function(costumeData) {
+                  // Now that we have returned, add the comment to the view.
+
+                  // get the new updated data.
+                  var costumeTotalRating = costumeData.totalRating;
+                  var costumeNumberRating = costumeData.numberRatings;
+
+                  // compute new number of stars
+                  var average = costumeTotalRating/costumeNumberRating;
+
+                  //show a new average rating
+                  $('#rating').barrating({
+                      theme: 'fontawesome-stars-o',
+                      showSelectedRating: false,
+                      initialRating: average
+                  });
+
+                  //disable rating after user already rated
+                  $('#rating').barrating('readonly', true);
+
+                }}, false);
+        }
+  });
+  //============================================================================
+}); // closes document ready.
+//==============================================================================
